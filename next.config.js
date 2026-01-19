79e94c541f8b61cd9ad6ceebaa7aae8ed1f58115
ShauryaChandna <75608229+ShauryaChandna@users.noreply.github.com>
2026-01-19 09:06:39 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/personal-webs",
-  assetPrefix: "/personal-webs/",
-};
-
-module.exports = nextConfig;
